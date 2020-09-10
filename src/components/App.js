1cc8ab7161e11ab088fca9c728a0564ca4de9151
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-10 22:42:20 +0200

    feat: import Calculate into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+// eslint-disable-next-line no-unused-vars
+import Calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
