77168e79d3217a03294fc67e9fbb1567c35a9664
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-10 12:50:27 +0200

    refactor: move App to components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div id="calcDisplay">
+        <Display answer="0" />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
+
+export default App;
