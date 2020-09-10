7947b4e0137663655926ed9de8b72ffdc6e47ab6
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-10 18:33:00 +0200

    style: add button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  const { name } = props;
+  const { name, wide, color } = props;
+  const width = wide ? '50%' : '25%';
+  const btnStyle = {
+    width,
+    background: color,
+  };
   return (
-    <button type="submit" className="button">
+    <button type="submit" className="button" style={btnStyle}>
       {name}
     </button>
   );
@@ -12,6 +17,18 @@ function Button(props) {
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+};
+
+Button.propTypes = {
+  wide: PropTypes.bool.isRequired,
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+};
+
+Button.defaultProps = {
+  color: '#f5913e',
 };
 
 export default Button;
