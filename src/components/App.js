a3b8fa619370cae1a346cea262e5187664bb3562
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-11 13:08:27 +0200

    feat: add state to app class

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
-import Calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      // eslint-disable-next-line react/no-unused-state
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const dataObj = this.state;
+    this.setState(calculate(dataObj, buttonName));
+  }
+
   render() {
+    const data = this.state;
     return (
       <div id="calcDisplay">
-        <Display answer="0" />
-        <ButtonPanel />
+        <Display answer={data.next || data.total || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
